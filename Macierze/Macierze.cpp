cf4bb1d40d28ba9548b71ef3879f1a72c0ac2d94
morrky89 <wiercioch1989@gmail.com>
2014-10-27 17:08:13 +0100

    First

---
+++ Macierze/Macierze.cpp
@@ -0,0 +1,22 @@
+#include "Macierze.h"
+#include <iostream>
+
+using namespace std;
+
+Macierze::Macierze()
+{
+
+}
+
+
+Macierze::~Macierze()
+{
+
+}
+
+
+int main() {
+
+	cout << "Oh" << endl;
+
+}
